20c4ae113c6fbcbeacfcdf2952a175a380d9cbc7
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-07 23:42:18 -0400

    Added columns to each model

--- models/index.js
+++ models/index.js
@@ -5,10 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: '',
+})
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {

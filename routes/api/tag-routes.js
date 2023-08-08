62ffa8badddc188f55e83d849b1fb2422e3a9d1a
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-08 03:04:28 -0400

    Added delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,14 +38,50 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  // Tag.create(req.body)
+  //   .then((tag) => {
+  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+  //     if (req.body.tagIds.length) {
+  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
+  //         return {
+  //           product_id: product.id,
+  //           tag_id,
+  //         };
+  //       });
+  //       return ProductTag.bulkCreate(productTagIdArr);
+  //     }
+  //     // if no product tags, just respond
+  //     res.status(200).json(product);
+  //   })
+  //   .then((productTagIds) => res.status(200).json(productTagIds))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(400).json(err);
+  //   });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if(!tagData) {
+      res.status(404).json({ message: 'No tag with that id.'});
+      return;
+    }
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

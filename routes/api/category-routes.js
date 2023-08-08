50c7aab03329f38127a3b6e16b3a33e36cd5c61f
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-08 00:25:20 -0400

    Added get all to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+// added async
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product}]
+    });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
